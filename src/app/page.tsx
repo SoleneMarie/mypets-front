ac5670c4cb1d8c0413e253f4f3adf365259d3218
SoleneMarie <solene.plassart@gmail.com>
2025-05-30 23:13:17 +0200

    style: page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,22 @@
 "use client";
-import { useRouter } from "next/navigation";
 
-import Button from "../../components/ui/button";
-import { PawPrint, UserPlus } from "lucide-react";
-import MainLogo from "../../components/ui/mainLogo";
+import { useRouter } from "next/navigation";
 import FetchButton from "../../components/ui/fetchButton";
+import Footer from "../../components/ui/footer";
 
 export default function Home() {
   const router = useRouter();
+
   return (
-    <div className="">
-      <MainLogo />
-      <Button
-        type="large"
-        text="Retour à l'accueil"
-        icon={<PawPrint />}
-        onClick={() => console.log("Clique sur adopter")}
-      />
-      <Button
-        type="header"
-        text="Animaux"
-        icon={<UserPlus className="w-5 h-5" />}
-        onClick={() => console.log("Clique sur créer un compte")}
-        selected={true}
-      />
-      <p className="text-[var(--highlight)]">Un texte doux</p>
-      <p className="text-[var(--foreground)]">Un texte foncé</p>
-      <p className="text-[var(--danger)]">Un avertissement</p>
-      <div className="flex flex-col w-full justify-center items-center">
+    <div className="flex flex-col min-h-screen">
+      {/* Contenu principal centré */}
+      <main className="flex flex-col flex-1 items-center justify-center">
         <h1 className="logo text-7xl text-[var(--danger)] mb-8">My Pets</h1>
         <FetchButton onClick={() => router.push("/home")} />
-      </div>
+      </main>
+
+      {/* Footer sticky en bas */}
+      <Footer />
     </div>
   );
 }
